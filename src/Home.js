b8231aa54f15c9270651a8c0e360591a00204f64
Barry Scott Rollan <barry.rollan@gmail.com>
2019-08-27 12:30:11 -0400

    increased the size of the sola img

--- src/Home.js
+++ src/Home.js
@@ -19,7 +19,7 @@ const Home = props => {
             <img src={OmBanner} alt='' style={{ width: '100%' }} />
             <div className="col-md-12 center-block" style={{ backgroundImage: 'linear-gradient(90deg, #343a40, #7c7c7c, #343a40)', padding: '12px', width: '100vw', margin: '0' }}>
                 <p className='pageHeader'>Barry is now working at the Dunwoody location of</p>
-                <img src={Sola} alt='' style={{ maxWidth: '86vw' }}/>
+                <img src={Sola} alt='' style={{ maxWidth: '86vw', minWidth: '30vw' }}/>
             </div>
             <p>4505 Ashford Dunwoody Rd 30346</p>
             <Map />
