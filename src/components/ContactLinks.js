cc6a3be49111d9c9deca927890c0ad666e67a96c
Barry Rollan <barry.rollan@gmail.com>
2019-12-26 11:06:31 -0500

    gave top and bottom margin on contact links div--borders were disappearing

--- src/components/ContactLinks.js
+++ src/components/ContactLinks.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const ContactLinks = () => {
-
-    return(
-        <div>
-            <a href="mailto:barry@barryrollanstudio.com" 
-            style={{ clear: 'both', display: 'inline-block' }}
-            id="emailLink"
-            >Email Barry
-            </a>
-            <a href="sms:4042023562"
-            style={{ display: 'none' }}
-            id="textLink"
-            >Text Barry
-        </a>
-        </div>
-
-        )
+  return (
+    <div style={{ margin: '5px 0' }}>
+      <a
+        href="mailto:barry@barryrollanstudio.com"
+        style={{ clear: "both", display: "inline-block" }}
+        id="emailLink"
+      >
+        Email Barry
+      </a>
+      <a href="sms:4042023562" style={{ display: "none" }} id="textLink">
+        Text Barry
+      </a>
+    </div>
+  );
 };
 
 export default ContactLinks;
